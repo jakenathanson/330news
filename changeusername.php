350413025a84dd04a7bd87eba4eea573d68cb6e6
Jake Nathanson <jake.nathanson@me.com>
2018-02-18 15:34:42 -0600

    hi

--- changeusername.php
+++ changeusername.php
@@ -3,12 +3,38 @@
 
 <?php
 require 'database.php';
-
+session_start();
 $first = $_POST['newUsername1'];
 $second = $_POST['newUsername2'];
+$uid=$_SESSION['uid'];
 
-if (!((strcmp($first, $second))==0)) {
-  echo("<script>alert(\"Your usernames did not match! Try again.\")")
+if (!$uid||!$first||!$second) {
+  header('Location: home.php');
+}
+
+
+if (((strcmp($first, $second))==0)) {
+
+  $newname=$first;
+
+  // update appropriate values in database
+  $stmt = $mysqli->prepare("update users set username=? where uid=?");
+  if (!$stmt) {
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+  	exit;
+  }
+  $stmt->bind_param('si', $newname, $uid);
+  $stmt->execute();
+  $stmt->close();
+  $_SESSION['user']=$newname;
+  // redirect to poststory
+  header('Location: home.php');
+
+} else {
+
+  echo("<script>alert(\"Your usernames did not match! Try again.\"); location=\"account.php\"</script>");
+
+
 }
 
 ?>
