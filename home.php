4c93d83b4c0cd15fb8b29989466062811016b1dc
Jake Nathanson <jake.nathanson@me.com>
2018-02-17 22:58:45 -0600

    often and early

--- home.php
+++ home.php
@@ -12,12 +12,13 @@
   session_start();
   if ($_SESSION['uid']){
     echo '<li style="float:right; background-color: red;"><a class="active" href="login.php">Logout</a></li>';
+    echo '<li style="float:right; background-color: green;"><a class="active" href="post-story.php">Post a Story</a></li>';
   } else {
     echo'<li style="float:right; background-color: green;"" ><a class="active" href="register.php">Register</a></li>';
     echo'<li style="float:right; background-color: green;"" ><a class="active" href="login.php">Login</a></li>';
   }
-
   ?>
+
   <li style="float:left"><a class="active" href="#about">330news</a></li>
 </ul>
 </body>
