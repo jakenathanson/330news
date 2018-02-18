a82309c22f1e0f34daff506c72cf0687b32f3d9c
Jake Nathanson <jake.nathanson@me.com>
2018-02-18 13:59:32 -0600

    account page

---
+++ account.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+  <ul>
+    <?php
+
+
+    session_start();
+
+    // if visitor is logged in, show "post story" and "logout" buttons
+    if (isset($_SESSION['uid'])){
+      echo '<li style="float:right; background-color: red;"><a class="active" href="destroy.php">Logout</a></li>';
+      echo '<li style="float:right; background-color: green;"><a class="active" href="home.php">Home</a></li>';
+    } else { // otherwise, show "login" and "register"
+      echo'<li style="float:right; background-color: green;"" ><a class="active" href="register.php">Register</a></li>';
+      echo'<li style="float:right; background-color: green;"" ><a class="active" href="login.php">Login</a></li>';
+    }
+    ?>
+
+    <li style="float:left"><a class="active" href="#about">330news</a></li>
+  </ul>
+
+  <div id="account_data">
+
+    <?php
+    <?php echo $_SESSION['user'];?>
+
+    require 'database.php';
+
+    // get UI contents of 'stories' table
+    $stmt = $mysqli->prepare("select storyid, title, author from stories");
+    if(!$stmt){
+      printf("Query Prep Failed: %s\n", $mysqli->error);
+      exit;
+    }
+    $stmt->execute();
+
+    // bind result, use array for display purposes
+    $result = $stmt->get_result();
+
+    // iteratively generate html table with database contents
+    while($row = $result->fetch_assoc()){
+      echo "<tr>";
+      printf("<td>%s</td>",$row["title"]);
+      printf("<td>%s</td>",$row["author"]);
+      // create form for viewing stories (storyid passed as post)
+      echo "<td><form action=\"storypage.php\" method=\"post\">";
+      echo "<input type=\"submit\" name=\"storyid\" value=\"Read\"/>";
+      printf("<input type=\"hidden\" name=\"storyid\" value=\"%s\">",$row["storyid"] );
+      echo "</form></td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+
+    $stmt->close();
+    ?>
+  </div>
+
+</body>
+</html>
