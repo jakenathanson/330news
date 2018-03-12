2c0e5bce080730171c707b74042595c72b0fe2f7
Jake Nathanson <35723005+jakenathanson@users.noreply.github.com>
2018-03-12 17:10:04 -0400

    Update database.php
    hide key

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 // Content of database.php
 
-$mysqli = new mysqli('localhost', 'server', 'RNJXTffZ3EcjD7bz', '330news');
+$mysqli = new mysqli('localhost', 'server', 'KEY', '330news');
 
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
